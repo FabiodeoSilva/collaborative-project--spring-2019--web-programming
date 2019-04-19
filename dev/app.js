51603a04181897e97850082b65c61fcbc9fc23f9
Fabio Silva <fabiodeosilva@gmail.com>
2019-04-19 18:35:25 -0400

    Add function checkStageTimeLimit
    checks if the time limit of all stages add up to 100%;

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,16 @@
-(function() {
-  let Haunter = require("haunter");
-  ("using namespace");
-  const haunter = new Haunter();
-  haunter.init();
-})();
+const Haunter = require("haunter");
+const Stage = require("stage");
+
+("using namespace");
+
+let say = () => {
+  console.log("say");
+};
+let cry = () => {
+  console.log("cry");
+};
+
+let sayStage = new Stage(0, 10, [say, cry]);
+
+const haunter = new Haunter(1, [sayStage]);
+haunter.init();
