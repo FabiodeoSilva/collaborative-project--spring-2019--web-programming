425c255be2c6964389daa5903ce6b98410b1a162
Fabio Silva <fabiodeosilva@gmail.com>
2019-04-25 21:21:44 -0400

    Add cursor class
    Draw cursor since we can't use images

--- dev/chrome-extesion-test/content.js
+++ dev/chrome-extesion-test/content.js
@@ -8,7 +8,48 @@
   });
 
   let setupCanvas = () => {
+    class Cursor {
+      constructor(type, positionCoor, speed, s) {
+        this.type = type;
+        this.speed = speed;
+        this.update(positionCoor[0], positionCoor[1], s);
+      }
+      draw(s) {
+        s.clear();
+        s.fill("black");
+        s.stroke("white");
+        s.beginShape();
+        this.vertices.forEach(coor => {
+          s.vertex(coor[0], coor[1]);
+        });
+        s.endShape(s.CLOSE);
+      }
+      update(x, y, s) {
+        this.vertices = [
+          [x, y],
+          [x, y + 16],
+          [x + 3.96, y + 12.6],
+          [x + 7.5, y + 21.43],
+          [x + 10, y + 20.1],
+          [x + 6.7, y + 12],
+          [x + 12.2, y + 12]
+        ];
+        this.draw(s);
+      }
+    }
+
     let p5Canvas = s => {
+      let macCursor,
+        winCursor,
+        i = 1;
+
+      /*s.preload = () => {
+        macCursor = s.loadImage("media/mac-cursor.png");
+        winCursor = s.loadImage(
+          "http://box5768.temp.domains/~fabiosil/wp-content/uploads/2019/04/windows-cursor.png"
+        );
+      };*/
+
       s.setup = () => {
         let h = document.body.clientHeight;
         c = s.createCanvas(s.windowWidth, s.windowHeight);
@@ -16,20 +57,23 @@
         c.style("pointer-events", "none");
         c.style("position", "fixed");
         c.style("z-index", 999);
+        // s.image(macCursor, 100, 100);
+        macCursor = new Cursor("mac", [100, 100], 100, s);
       };
 
       s.draw = () => {
-        s.stroke("red");
-        s.noFill();
+        macCursor.update(i, i * 2, s);
+        i++;
       };
 
       s.mouseMoved = () => {
-        s.clear();
-        s.ellipse(s.mouseX, s.mouseY, 80);
+        //s.image(macCursor, s.mouseX - 5, s.mouseY - 10);
       };
     };
     let myp5 = new p5(p5Canvas);
   };
+
+  let cursorSwarm = amount => {};
   setupCanvas();
 
   let activateCursedImage = (imgUrl, destinationURL) => {
