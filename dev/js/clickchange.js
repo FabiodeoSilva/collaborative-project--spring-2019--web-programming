339a7de986a5711f39b3788dd5ec4ad65b95262d
Catherine <catherinelu97@gmail.com>
2019-05-03 17:04:29 -0400

    added zalgo to html and css

---
+++ dev/js/clickchange.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(`#symbol`).click(function(){
+        $(`body`).css({"background-color": "yellow"
+        });
+        $(`h1`).css({"color": "black"
+        });
+        $(`.zalgo`).css({"color": "red"
+        });
+        $(`.typewriter`).css({"color": "purple"
+        });
+    });
+});
