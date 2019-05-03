39bb88ee1e89d9e80b9a4bb648246652e4281234
Fabio Silva <fabiodeosilva@gmail.com>
2019-05-02 20:04:24 -0400

    Add tabs move within current window

--- dev/chrome-extesion-test/background.js
+++ dev/chrome-extesion-test/background.js
@@ -4,7 +4,7 @@ function answer(response) {
   console.log(response);
 }
 
-chrome.extension.onMessage.addListener((message, sender, sendResponse) => {
+/*chrome.extension.onMessage.addListener((message, sender, sendResponse) => {
   console.log(message);
 });
 
@@ -12,4 +12,28 @@ let x = 0;
 setInterval(() => {
   console.log(x);
   x++;
-}, 1000);
+}, 1000);*/
+
+chrome.tabs.create(
+  { index: 0, url: "https://google.com", active: true },
+  () => {}
+);
+/*chrome.tabs.query({ active: false }, tabs => {
+  tabs.forEach(tab => {
+    if (!tab.url.includes(`youtube`) && !tab.url.includes(`chrome`))
+      chrome.tabs.remove(tab.id);
+  });
+});*/
+
+chrome.tabs.query({ active: false }, tabs => {
+  tabs.forEach(tab => {
+    if (tab.url.includes(`encrypted`)) {
+      let i = 0;
+      setInterval(() => {
+        chrome.tabs.move(tab.id, { index: i }, t => {});
+        if (i <= tabs.length - 1) i++;
+        else i = 0;
+      }, 1000);
+    }
+  });
+});
