d776e213c914b9f1e3867a59c0112249cc9b2f18
Catherine <catherinelu97@gmail.com>
2019-05-04 15:24:54 -0400

    tiny modifications

--- dev/js/clickchange.js
+++ dev/js/clickchange.js
@@ -22,9 +22,9 @@ $(document).ready(function(){
                         scale: true, // set 'false' to stop scaling
                         blend : true, // set 'false' to stop glitch blending
                         blendModeType : 'hue', // select blend mode type
-                        glitch1TimeMin : 1600, // set min time for glitch 1 elem
+                        glitch1TimeMin : 600, // set min time for glitch 1 elem
                         glitch1TimeMax : 900, // set max time for glitch 1 elem
-                        glitch2TimeMin : 100, // set min time for glitch 2 elem
+                        glitch2TimeMin : 10, // set min time for glitch 2 elem
                         glitch2TimeMax : 115, // set max time for glitch 2 elem
                         zIndexStart : -8, // because of absolute position, set z-index base value
 		});
