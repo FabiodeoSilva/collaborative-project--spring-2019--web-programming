012efc0e1f6b8f14deee5eeb29077f6fac698a24
Fabio Silva <fabiodeosilva@gmail.com>
2019-05-03 22:41:34 -0400

    Update content.js

--- dev/chrome-extesion-test/content.js
+++ dev/chrome-extesion-test/content.js
@@ -7,31 +7,6 @@
     console.log(response);
   });
 
-  let setupCanvas = () => {
-    let p5Canvas = s => {
-      s.setup = () => {
-        let h = document.body.clientHeight;
-        c = s.createCanvas(s.windowWidth, s.windowHeight);
-        c.position(0, 0);
-        c.style("pointer-events", "none");
-        c.style("position", "fixed");
-        c.style("z-index", 999);
-      };
-
-      s.draw = () => {
-        s.stroke("red");
-        s.noFill();
-      };
-
-      s.mouseMoved = () => {
-        s.clear();
-        s.ellipse(s.mouseX, s.mouseY, 80);
-      };
-    };
-    let myp5 = new p5(p5Canvas);
-  };
-  setupCanvas();
-
   let activateCursedImage = (imgUrl, destinationURL) => {
     let imgsArr = document.querySelectorAll("img");
     let bigImgs = [];
@@ -59,8 +34,8 @@
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  /*activateCursedImage(
+  activateCursedImage(
     "https://vignette.wikia.nocookie.net/dreamworks/images/5/54/Rico03.png/revision/latest?cb=20150717211446",
     "https://github.com/"
-  );*/
+  );
 })();
