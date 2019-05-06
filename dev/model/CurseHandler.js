9b056d6274323bdf4f6c7bc2cc5a91a1656449a2
Fabio Silva <fabiodeosilva@gmail.com>
2019-05-06 19:52:14 -0400

    Add p5 to curseHandler

--- dev/model/CurseHandler.js
+++ dev/model/CurseHandler.js
@@ -3,7 +3,9 @@ class CurseHandler {
     this.curseArr = curseArr;
   }
   init() {
-    this.listenForActivation();
+    //this.listenForActivation();
+    this.canvasSetUp();
+    this.draw();
   }
   listenForActivation() {
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -23,6 +25,29 @@ class CurseHandler {
     } else if (key[0] == `message`) {
       //console.log(request.message);
     }
+  }
+
+  canvasSetUp() {
+    let p5Canvas = s => {
+      console.log(s);
+
+      s.setup = () => {
+        let h = document.body.clientHeight;
+        s.createCanvas(s.windowWidth, s.windowHeight);
+        /*c.position(0, 0);
+        c.style("pointer-events", "none");
+        c.style("position", "fixed");
+        c.style("z-index", 999);*/
+      };
+    };
+    this.myp5 = new p5(p5Canvas);
+  }
+  draw() {
+    let s = this.myp5;
+    s.draw = () => {
+      s.fill("black");
+      s.ellipse(100, 100, 100);
+    };
   }
 }
 
